e46bb4aea310c707e6058a71c5cd928beb6bc8b3
Robert Brito <robert@gmail.com>
2023-04-18 21:10:12 -0300

    Criado aula 3 em python

---
+++ aula 3.py
@@ -0,0 +1,6 @@
+nota1 = float(input("informe a nota 1: ")) 
+nota2 = float(input("informe a nota 2: "))
+nota3 = float(input("informe a nota 3: "))
+#couculo da media
+media = (nota1 + nota2 + nota3) / 3
+print(f"a media final é {media}\n")
