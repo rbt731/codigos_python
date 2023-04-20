0a26d98fe23b517c4a6f219a9da557f7cceb9f60
Robert Brito <robert@gmail.com>
2023-04-19 21:15:06 -0300

    faltando a 4,5

---
+++ Atividade_sequencial/Atividade_4Q.py
@@ -0,0 +1,8 @@
+lata = int(input("Digite a quantidade de latas de 300ml de refrigerante: "))
+garrafaml = int(input("Digite quantas garrafas de 600ml: "))
+garrafal = int(input("Digite quantas garrafas de 2L: "))
+
+litroA = lata*300
+print("Total 300ml: ", N1)
+print("Total 300ml: ", garrafaml)
+print("Total 300ml: ", garrafal)
