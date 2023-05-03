fa836676c1e0e75ac930bf7178604354c4bad70e
Robert Brito <robert@gmail.com>
2023-05-03 19:37:51 -0300

    atualizado

--- aula7-enquanto.py
+++ aula7-enquanto.py
@@ -5,14 +5,15 @@ while contador <=10:
     contador = contador + 1
 
 #while funcionando como loop condicional
-"""
+
 senha = ""
 while senha != "123":
     senha = input("Informe sua senha: ")
 
 print("Obrigado por usar o sistema\n")
-"""
+
 #while como se fosse o faça - enquanto
+
 while True: 
     senha = input("informe sua senha: ")
     if senha == "123":
