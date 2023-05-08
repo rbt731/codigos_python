32f8befb4b45700380b693f926828ad02e5f1a7f
Robert Brito <robert@gmail.com>
2023-05-08 19:38:03 -0300

    Finalizado atividade repetição

--- 5Atividade_repetição/atividade3.py
+++ 5Atividade_repetição/atividade3.py
@@ -1,26 +1,25 @@
-maior = 0
-menor = 0
-soma = 0
-soma1 = 0
-s = 0
-i = 0 
-while i <= 6:
+somaTurma = 0
+somaMulheres = 0
+contMulheres = 0
+for i in range(1,7):
     altura = float(input("Informe sua altura: "))
     sexo = int(input("Informe seu sexo: "))
-    if i == 0:
+    if i == 1:
         menor = altura
+        maior = altura
     else:
         if altura >= maior:
             maior = altura
+        
         if altura <= menor:
             menor = altura
+    
     if sexo == 2:
-        soma += altura
-        s += 1
-    soma1 += altura
-    mediatotal = soma1 / 6
-    i += 1
-mediaM = soma / s
-print(maior)
-print(menor)
-print(mediaM)
+        somaMulheres += altura
+        contMulheres += 1
+
+    somaTurma += altura
+    
+print(f"A maior altura é {maior} e a menor altura é {menor}")
+print(f"A média de alturas das mulheres é {somaMulheres / contMulheres}")
+print(f"A média de altura da turma é {somaTurma / 6}\n")
