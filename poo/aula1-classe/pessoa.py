6174eca8f0cbb707130417f491fb8959d13c8efd
robert <robertotaku233@gmail.com>
2023-05-18 21:04:03 -0300

    .

---
+++ poo/aula1-classe/pessoa.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    # atributos
+    nome = "Fulano"
+    idade = 30
+    altura = 1.65
+
+pessoa1 = Pessoa()
+print(pessoa1.nome, pessoa1.idade)# instanciar é cria um objeto a partir de uma classe.
